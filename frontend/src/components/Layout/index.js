7e89469c92c92188c555590cea624b893e6c3f6e
Suman Lama <sumandomarpa@gmail.com>
2019-03-28 14:59:44 +1100

    adds login form design

---
+++ frontend/src/components/Layout/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import {
+  Layout, Menu, Breadcrumb, Icon,
+} from 'antd';
+import { Link } from "react-router-dom";
+
+import { Logo } from './styles'
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+export default class DashboardLayout extends Component {
+  render () {
+    const { children } = this.props
+    return (
+      <Layout>
+        <Header className="header">
+          <Logo />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="3">Logout</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} style={{ background: '#fff' }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" title={<span><Icon type="dashboard" />Dashboard</span>}>
+                <Menu.Item key="1">
+                  <Link to="/dashboard">Dashboard</Link>
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" title={<span><Icon type="copy" />Pages</span>}>
+                <Menu.Item key="2">
+                  <Link to="/dashboard/pages">All Pages</Link>
+                </Menu.Item>
+                <Menu.Item key="3">Add New</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" title={<span><Icon type="laptop" />Faqs</span>}>
+                <Menu.Item key="4">All Faqs</Menu.Item>
+                <Menu.Item key="5">Add New</Menu.Item>
+                <Menu.Item key="6">Faq Categories</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" title={<span><Icon type="notification" />Article / News</span>}>
+                <Menu.Item key="7">All Articles / News</Menu.Item>
+                <Menu.Item key="8">Add New</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content style={{
+              background: '#fff', padding: 24, margin: 0, minHeight: 280,
+            }}
+            >
+              {children}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    )
+  }
+}
