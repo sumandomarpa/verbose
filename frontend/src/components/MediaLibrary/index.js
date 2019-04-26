bd5a9bf6e8d59d35904a8036a288024dfc6d9331
Suman Lama <sumandomarpa@gmail.com>
2019-04-26 10:22:49 +1000

    allows user to select an image on frontend

--- frontend/src/components/MediaLibrary/index.js
+++ frontend/src/components/MediaLibrary/index.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
 
 import Layout from '../Layout'
 import AddMedia from './Add'
 import ListMedia from './List'
 import { ListMediaWrapper } from './styles'
 
-export default class MediaLibrary extends Component {
+class MediaLibrary extends Component {
   render() {
-    return (
-      <Layout>
+    const { includeLayout, onMediaSelect } = this.props
+
+    const content = (
+      <Fragment>
         <AddMedia />
         <ListMediaWrapper>
-          <ListMedia />
+          <ListMedia onMediaSelect={onMediaSelect} />
         </ListMediaWrapper>
-      </Layout>
+      </Fragment>
     )
+    const renderMediaLibrary = includeLayout ? (
+      <Layout>{content}</Layout>
+    ) : (
+      content
+    )
+    return <Fragment>{renderMediaLibrary}</Fragment>
   }
 }
+
+MediaLibrary.defaultProps = {
+  includeLayout: true,
+}
+
+MediaLibrary.propTypes = {
+  includeLayout: PropTypes.bool,
+  onMediaSelect: PropTypes.func,
+}
+
+export default MediaLibrary
--- frontend/src/components/Pages/Add/Sections/Block/index.js
+++ frontend/src/components/Pages/Add/Sections/Block/index.js
@@ -1,15 +1,19 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { Form, Input, Select } from 'antd'
+import { Form, Input, Select, Button, Modal } from 'antd'
 import { Query, withApollo } from 'react-apollo'
-import TinyMCEditor from '../../../../TinyMCEditor'
 
+import MediaLibrary from '../../../../MediaLibrary'
+import TinyMCEditor from '../../../../TinyMCEditor'
 import { GET_BLOCK } from '../../../queries'
 import { UPDATE_BLOCK } from '../../../mutaitons'
+import { MediaImage } from './styles'
 
 const { Option } = Select
 
 class Block extends Component {
+  state = { visible: false, media: {} }
+
   handleInputChange = (e, name, value) => {
     const { client, itemId } = this.props
 
@@ -23,13 +27,42 @@ class Block extends Component {
     })
   }
 
+  selectImage = () => {
+    console.log('select image')
+    this.setState({ visible: true })
+  }
+
+  handleOk = e => {
+    const { media } = this.state
+    this.setState({
+      visible: false,
+    })
+  }
+
+  handleCancel = e => {
+    this.setState({
+      visible: false,
+      media: {},
+    })
+  }
+
+  onMediaSelect = media => {
+    this.setState({ media })
+  }
+
   render() {
     const { itemId } = this.props
+    const { visible } = this.state
     return (
       <Query query={GET_BLOCK} variables={{ itemId }}>
         {({ data: { block }, loading }) => {
           if (loading) return null
           const { title, image, video, style, content } = block
+
+          const {
+            media: { url },
+          } = this.state
+          const renderMedia = url ? <MediaImage src={url} /> : null
           return (
             <Fragment>
               <Form.Item label="Title">
@@ -49,6 +82,26 @@ class Block extends Component {
                   onChange={this.handleInputChange}
                 />
               </Form.Item>
+              <Form.Item label="Image">
+                {renderMedia}
+                <Button onClick={this.selectImage}>Select Image</Button>
+              </Form.Item>
+
+              <Modal
+                title="Select an Image"
+                visible={visible}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+                width={1400}
+                style={{ top: 10 }}
+                okText="Insert Image"
+              >
+                <MediaLibrary
+                  includeLayout={false}
+                  onMediaSelect={this.onMediaSelect}
+                />
+              </Modal>
+
               <Form.Item label="Video">
                 <Input
                   name="video"
