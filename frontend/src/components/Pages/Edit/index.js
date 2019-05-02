7ad7d81ecbc98021f0c3c8fc8a27dfae320a3d8a
Suman Lama <sumandomarpa@gmail.com>
2019-05-02 16:01:12 +1000

    remove unused page code

--- frontend/src/components/Pages/Edit/index.js
+++ frontend/src/components/Pages/Edit/index.js
@@ -4,23 +4,14 @@ import { withApollo } from 'react-apollo'
 import PropTypes from 'prop-types'
 import omit from 'lodash/omit'
 import sortBy from 'lodash/sortBy'
-import findIndex from 'lodash/findIndex'
-import assign from 'lodash/assign'
 
 import Layout from '../../Layout'
 import PageForm from '../Add/PageForm'
 import SortableList from '../Add/SortableList'
 import PageItem from '../Add/AddPageItem'
 import { ActionButtonsWrapper, AddNewPageWrapper } from '../Add/styles'
-import {
-  GET_PAGE_DB,
-  GET_PAGE,
-  GET_BLOCKS,
-  GET_BOXES,
-  GET_PROS_AND_CONS,
-  GET_PAGE_ITEMS,
-} from '../queries'
-import { UPDATE_PAGE_TO_DB, UPSERT_PAGE_TO_DB } from '../mutaitons'
+import { GET_PAGE_DB, GET_PAGE } from '../queries'
+import { UPSERT_PAGE_TO_DB } from '../mutaitons'
 
 class EditPage extends Component {
   state = { loading: false }
@@ -51,71 +42,6 @@ class EditPage extends Component {
     if (upsertPage.id) {
       message.success('Page updated successfully')
     } else message.error('Error! Page update failed')
-  }
-
-  handleSubmit = async () => {
-    const { client } = this.props
-    const { page } = client.readQuery({
-      query: GET_PAGE,
-    })
-
-    const { pageItems } = client.readQuery({
-      query: GET_PAGE_ITEMS,
-    })
-
-    const { blocks } = client.readQuery({
-      query: GET_BLOCKS,
-    })
-    const trimBlocks = blocks.map(block =>
-      assign(omit(block, ['__typename']), {
-        order: findIndex(pageItems, pageItem => pageItem.itemId === block.id),
-      })
-    )
-
-    const { boxes } = client.readQuery({
-      query: GET_BOXES,
-    })
-    const trimBoxes = boxes.map(box =>
-      assign(omit(box, ['__typename']), {
-        order: findIndex(pageItems, pageItem => pageItem.itemId === box.id),
-      })
-    )
-
-    const { prosAndCons } = client.readQuery({
-      query: GET_PROS_AND_CONS,
-    })
-    const trimProsAndCons = prosAndCons.map(elem => {
-      const data = assign(omit(elem, ['__typename']), {
-        order: findIndex(pageItems, pageItem => pageItem.itemId === elem.id),
-      })
-      data.pros = data.pros.map(elem => omit(elem, ['__typename']))
-      data.cons = data.cons.map(elem => omit(elem, ['__typename']))
-      return data
-    })
-
-    try {
-      const {
-        data: { updatePage },
-      } = await client.mutate({
-        mutation: UPDATE_PAGE_TO_DB,
-        variables: {
-          id: page.id,
-          title: page.title,
-          slug: page.slug,
-          type: page.type,
-          vertical: page.vertical,
-          blocks: trimBlocks,
-          boxes: trimBoxes,
-          prosAndCons: trimProsAndCons,
-        },
-      })
-      if (updatePage.id) {
-        console.log('updated successfully')
-        this.fetchFromDBtoCache()
-      }
-    } catch (e) {
-      console.log('update failed')
-    }
   }
 
   async fetchFromDBtoCache() {
